9fdd63a81191da4dcbccb692d9e8e0f0c3fd5f5a
Bharat Manani <k250165@nu.edu.pk>
2025-10-23 09:27:35 +0500

    Add files via upload

---
+++ task 01 nested loop.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main(){
+int i=0;
+int j=0;
+// Number of tables
+ for(i = 1; i<6; i++) {
+ 	printf("%d: ",i);
+ 	for(j = 1; j<5; j++)
+ 	  
+ 	  	printf("%3d  ",i*j);
+ 	  	printf("\n");
+	   }	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
